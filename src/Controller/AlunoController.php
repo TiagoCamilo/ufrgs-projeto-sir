9bd88a680cb49bb90f84b2c6dd9955573091fa13
Tiago Camilo <camilo.tiago@gmail.com>
2018-09-25 14:15:22 -0300

    Removendo comentarios

--- src/Controller/AlunoController.php
+++ src/Controller/AlunoController.php
@@ -33,17 +33,10 @@ class AlunoController extends AbstractController
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            // $form->getData() holds the submitted values
-            // but, the original `$task` variable has also been updated
             $aluno = $form->getData();
             dump($aluno);
             die();
 
-            // ... perform some action, such as saving the task to the database
-            // for example, if Task is a Doctrine entity, save it!
-            // $entityManager = $this->getDoctrine()->getManager();
-            // $entityManager->persist($task);
-            // $entityManager->flush();
 
             return $this->redirectToRoute('home');
         }
