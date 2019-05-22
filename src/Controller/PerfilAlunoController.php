81792915c23b9668b66714e9ed4fd0906c309eaa
Tiago Camilo <camilo.tiago@gmail.com>
2019-05-22 12:01:09 -0300

    Implementado seach dos alunos na tela de home

--- src/Controller/PerfilAlunoController.php
+++ src/Controller/PerfilAlunoController.php
@@ -42,7 +42,4 @@ class PerfilAlunoController extends AppAbstractController
             'template' => (array) $this->getTemplateManager(),
         ]);
     }
-
-
-
 }
--- src/Migrations/Version20190520150501.php
+++ src/Migrations/Version20190520150501.php
@@ -12,25 +12,25 @@ use Doctrine\Migrations\AbstractMigration;
  */
 final class Version20190520150501 extends AbstractMigration
 {
-    public function getDescription() : string
+    public function getDescription(): string
     {
         return '';
     }
 
-    public function up(Schema $schema) : void
+    public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
+        $this->abortIf('postgresql' !== $this->connection->getDatabasePlatform()->getName(), 'Migration can only be executed safely on \'postgresql\'.');
 
         $this->addSql('ALTER TABLE formulario_registro ADD formulario_id INT NOT NULL');
         $this->addSql('ALTER TABLE formulario_registro ADD CONSTRAINT FK_CF48CD5441CFE234 FOREIGN KEY (formulario_id) REFERENCES formulario (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
         $this->addSql('CREATE INDEX IDX_CF48CD5441CFE234 ON formulario_registro (formulario_id)');
     }
 
-    public function down(Schema $schema) : void
+    public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
+        $this->abortIf('postgresql' !== $this->connection->getDatabasePlatform()->getName(), 'Migration can only be executed safely on \'postgresql\'.');
 
         $this->addSql('CREATE SCHEMA public');
         $this->addSql('ALTER TABLE formulario_registro DROP CONSTRAINT FK_CF48CD5441CFE234');
