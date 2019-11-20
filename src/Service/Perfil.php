358a8f5613f23a7b5a9b67692234a4f1c891552f
Tiago Camilo <camilo.tiago@gmail.com>
2019-11-20 15:12:27 -0300

    Refactor - Service Perfil, remoção de código obsoleto

--- src/Service/Perfil.php
+++ src/Service/Perfil.php
@@ -2,76 +2,17 @@
 
 namespace App\Service;
 
+use App\Entity\PerfilControleAcao;
 use App\Entity\Usuario;
 
 class Perfil
 {
-    public static $VIEW = 'VIEW';
-    public static $EDIT = 'EDIT';
-    public static $LIST = 'LIST';
-    public static $NEW = 'NEW';
-    public static $DELETE = 'DELETE';
-
     public static function checkUserPermission(Usuario $user, string $permission): bool
     {
-        $exists = $user->getPerfil()->getPerfilControleAcoes()->exists(function ($key, $element) use ($permission) {
+        $exists = $user->getPerfil()->getPerfilControleAcoes()->exists(function ($key, PerfilControleAcao $element) use ($permission) {
             return $permission === $element->getRoute();
         });
 
         return $exists;
-    }
-
-    /**
-     * @return array
-     */
-    public static function getSupportedActions()
-    {
-        return [
-            self::getVIEW(),
-            self::getEDIT(),
-            self::getLIST(),
-            self::getNEW(),
-            self::getDELETE(),
-        ];
-    }
-
-    /**
-     * @return string
-     */
-    public static function getVIEW(): string
-    {
-        return self::$VIEW;
-    }
-
-    /**
-     * @return string
-     */
-    public static function getEDIT(): string
-    {
-        return self::$EDIT;
-    }
-
-    /**
-     * @return string
-     */
-    public static function getLIST(): string
-    {
-        return self::$LIST;
-    }
-
-    /**
-     * @return string
-     */
-    public static function getNEW(): string
-    {
-        return self::$NEW;
-    }
-
-    /**
-     * @return string
-     */
-    public static function getDELETE(): string
-    {
-        return self::$DELETE;
     }
 }
